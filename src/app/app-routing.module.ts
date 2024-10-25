08b44893568dccfae0790e9cce334c18004083cc
Valentin Goxhaj <valentinogoxhaj@gmail.com>
2024-10-25 17:32:13 +0200

    updated rotuing empty path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
-    path: 'auth',
+    path: '',
     loadChildren: () =>
       import('./auth/auth.module').then((m) => m.AuthModule),
   },
